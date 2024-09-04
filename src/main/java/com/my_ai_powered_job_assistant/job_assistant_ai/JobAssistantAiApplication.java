79aa22da6704f702851e40119920630db4800338
M-Isak <mohamedisak.ali@gmail.com>
2024-09-04 17:07:24 +0100

    initial commit

---
+++ src/main/java/com/my_ai_powered_job_assistant/job_assistant_ai/JobAssistantAiApplication.java
@@ -0,0 +1,13 @@
+package com.my_ai_powered_job_assistant.job_assistant_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAssistantAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAssistantAiApplication.class, args);
+	}
+
+}
